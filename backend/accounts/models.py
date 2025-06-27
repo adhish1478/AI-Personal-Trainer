088100449b6051643522ce2bf0c2f8eddc2df932
adhish1478 <adhish.aravind7@gmail.com>
2025-06-27 20:36:55 +0530

    user profile model added

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -33,3 +33,55 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class UserProfile(models.Model):
+
+    GENDER_CHOICES = [('male', 'Male'), ('female', 'Female'), ('other', 'Other')]
+
+    user= models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    first_name= models.CharField(max_length=30, blank=True)
+    last_name= models.CharField(max_length=30, blank=True)
+    age= models.PositiveIntegerField(null=True, blank=True)
+    gender= gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+
+    height= models.FloatField() # in cm
+    weight= models.FloatField() # in kg
+    maintanance_cals= models.IntegerField(null= True)
+    goal_cals= models.IntegerField(null=True)
+    body_fat= models.FloatField(null=True, blank=True)
+    carb= models.FloatField(null=True)
+    protein= models.FloatField(null=True)
+    fat= models.FloatField(null=True)
+    fibre= models.FloatField(null=True)
+    allergies= models.JSONField(default=list, blank=True)
+    cuisine= models.CharField(max_length=50, blank=True, null=True)
+
+    created_at= models.DateField(auto_now_add=True)
+    updated_at= models.DateField(auto_now= True)
+
+    def __str__(self):
+        return f"{self.user.email}'s Profile"
+    
+    class Meta:
+        verbouse_name= "User Profile"
+        verbouse_name_plural= "User Profiles"
+
+    def calculate_maintanance_calories(self):
+        """
+        Calculates BMR (Basal Metabolic Rate) using the Mifflin-St Jeor Equation.
+        Returns estimated daily maintenance calories assuming sedentary activity.
+        """
+        if not self.weight or self.height or self.age or self.gender:
+            return None # Incomplete data
+        
+        weight= self.weight
+        height= self.height
+        age= self.age
+
+        if self.gender == 'female':
+            bmr= 10 * weight + 6.25 * height - 5 * age - 161
+        else:
+            bmr= 10 * weight + 6.25 * height - 5 * age + 5
+
+        activity_factor= 1.2 # For sedentary lifestyle, can be changed later
+        return(bmr*activity_factor)
