19fe53729684490ff6e335f2515ef14fc327c199
adhish1478 <adhish.aravind7@gmail.com>
2025-06-28 16:31:29 +0530

    model profile added

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -37,6 +37,13 @@ class CustomUser(AbstractUser):
 class UserProfile(models.Model):
 
     GENDER_CHOICES = [('male', 'Male'), ('female', 'Female'), ('other', 'Other')]
+    ACTIVITY_LEVEL_CHOICES = [
+        ('sedentary', 'Sedentary (little or no exercise)'),
+        ('lightly_active', 'Lightly active (light exercise/sports 1-3 days/week)'),
+        ('moderately_active', 'Moderately active (moderate exercise/sports 3-5 days/week)'),
+        ('very_active', 'Very active (hard exercise/sports 6-7 days a week)'),
+        ('extra_active', 'Extra active (very hard exercise/sports & physical job or training twice a day)')
+    ]
 
     user= models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
     first_name= models.CharField(max_length=30, blank=True)
@@ -46,6 +53,7 @@ class UserProfile(models.Model):
 
     height= models.FloatField() # in cm
     weight= models.FloatField() # in kg
+    activity_level= models.CharField(max_length=20, choices=ACTIVITY_LEVEL_CHOICES, default='sedentary')
     maintanance_cals= models.IntegerField(null= True)
     goal_cals= models.IntegerField(null=True)
     body_fat= models.FloatField(null=True, blank=True)
@@ -63,15 +71,25 @@ class UserProfile(models.Model):
         return f"{self.user.email}'s Profile"
     
     class Meta:
-        verbouse_name= "User Profile"
-        verbouse_name_plural= "User Profiles"
+        verbose_name= "User Profile"
+        verbose_name_plural= "User Profiles"
+
+    def get_activity_factor(self):
+        level_map= {
+            'sedentary': 1.2,
+            'lightly_active': 1.375,
+            'moderate': 1.55,
+            'active': 1.725,
+            'very_active': 1.9
+        }
+        return level_map.get(self.activity_level, 1.2) # Default to sedentary if not found
 
     def calculate_maintanance_calories(self):
         """
         Calculates BMR (Basal Metabolic Rate) using the Mifflin-St Jeor Equation.
         Returns estimated daily maintenance calories assuming sedentary activity.
         """
-        if not self.weight or self.height or self.age or self.gender:
+        if not all([self.weight, self.height, self.age, self.gender]):
             return None # Incomplete data
         
         weight= self.weight
@@ -83,5 +101,9 @@ class UserProfile(models.Model):
         else:
             bmr= 10 * weight + 6.25 * height - 5 * age + 5
 
-        activity_factor= 1.2 # For sedentary lifestyle, can be changed later
+        activity_factor= self.get_activity_factor()
         return(bmr*activity_factor)
+    
+    def save(self, *args, **kwargs):
+        self.maintanance_cals= self.calculate_maintanance_calories()
+        super().save(*args, **kwargs)
