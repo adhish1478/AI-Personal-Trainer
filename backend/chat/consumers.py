f7be285b921ed5b145cc8cd4ac9d9e2e7e6789ed
adhish1478 <adhish.aravind7@gmail.com>
2025-07-01 20:23:36 +0530

    gemini api added, predefined ques added, js looks for both json and markdown

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -29,11 +29,51 @@ class ChatBot(AsyncWebsocketConsumer):
         if self.question_index < len(PREDEFINED_QUESTIONS):
             await self.send_question()
         else:
-            await self.send(text_data= json.dumps({
-                'message': 'Thank you, generating a personalized plan...',
-                'answers': self.answers
-            }))
-        # LLM logic here
+            # If all questions are answered, process the answers
+            user_details = "\n".join(
+            f"{q} {a}" for q, a in zip(PREDEFINED_QUESTIONS, self.answers)
+            )
+
+            prompt= f"""Based on the following user information, recommend a personalized fitness plan:
+
+            {user_details}
+             
+
+            """
+
+            # LLm logic here
+            from google import genai
+            from django.conf import settings
+            import re
+            
+            client = genai.Client(
+                api_key=settings.GEMINI_API_KEY
+            )
+            response= client.models.generate_content(
+                model="gemini-2.5-flash", contents= prompt,
+            )
+
+            text= (
+                response.candidates[0].content.parts[0].text
+                if response.candidates and response.candidates[0].content.parts
+                else "No response from AI."
+            )
+            #print("RAW AI RESPONSE:", text)
+            '''try:
+                # Remove triple backticks and optional json tag using regex
+                #cleaned_text= re.sub(r'^```json\s*|\s*```', '', text.strip(), flags= re.MULTILINE).strip()
+
+                parsed= json.loads(text)
+            except Exception:
+                explanation= 'Invalid response from AI.' '''
+
+            # Send the AI response back to the client
+            await self.send(text_data=json.dumps({
+            "message": text,
+            "from_llm": True
+             }))
+
+            
 
     async def send_question(self):
         if self.question_index < len(PREDEFINED_QUESTIONS):
