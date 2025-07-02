3c0b93fa5c811c15c35009ed6b75a97e82cd172e
adhish1478 <adhish.aravind7@gmail.com>
2025-07-02 15:23:20 +0530

    chat models added and integrated it inside consumers

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class ChatMessage(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_from_user = models.BooleanField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{'User' if self.is_from_user else 'LLM'}: {self.message[:40]}"
