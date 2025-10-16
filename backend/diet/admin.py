5bda6c82836863cd1a6052aa6d8add8cc56cb148
adhish1478 <adhish.aravind7@gmail.com>
2025-10-16 17:25:16 +0530

    frontend react and tailwind init

--- backend/diet/admin.py
+++ backend/diet/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from .models import FoodItem, DailyMeal, MealIngredient
 # Register your models here.
+
+admin.site.register(FoodItem)
+
+# ---- Meal ingredient -----
+class MealIngredientAdmin(admin.ModelAdmin):
+    list_display = ('meal', 'name', 'quantity', 'get_user','meal__id', 'meal__source')
+
+    def get_user(self, obj):
+        return obj.meal.user.email
+    get_user.short_description = 'User'
+
+admin.site.register(MealIngredient, MealIngredientAdmin)
+
+# ---- DailyMeal- Tabular view----
+
+class MealIngredientInline(admin.TabularInline):
+    model= MealIngredient
+    extra= 1
+class DailyMealAdmin2(admin.ModelAdmin):
+    inlines = [MealIngredientInline]
+    list_display = ('user', 'meal_type', 'source','date')
+    search_fields = ('user__email',)
+    ordering = ('date',)
+    list_filter = ('source', 'date')
+admin.site.register(DailyMeal, DailyMealAdmin2)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline hover:text-blue-600">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
