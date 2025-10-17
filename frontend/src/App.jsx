e7b1f2d33bb663d6181e94ff490690b9e225947e
adhish1478 <adhish.aravind7@gmail.com>
2025-10-17 17:06:54 +0530

    Toaster added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Toaster />
       <Home />
     </>
   );
